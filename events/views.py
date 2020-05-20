9b4f6529cfbd767a38e3c42529d94a1b6f3abda2
codebox <anikraju110@gmail.com>
2020-05-21 03:29:31 +0600

    add pagination in event

--- events/views.py
+++ events/views.py
@@ -3,16 +3,28 @@ from django.views.generic import DetailView
 from .models import *
 import datetime
 
+from django.contrib.auth.models import User
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 # Create your views here.
 
 def events(request):
+	event_list = Event.objects.all().order_by('-date')
+	page = request.GET.get('page', 1)
+	paginator = Paginator(event_list, 1)
+	try:
+		events = paginator.page(page)
+	except PageNotAnInteger:
+		events = paginator.page(1)
+	except EmptyPage:
+		events = paginator.page(paginator.num_pages)
 	context = {
 
-		'events': Event.objects.all().order_by('-date'),
+		'events': events,
 		'status': 'all',
 		'today': datetime.date.today()
 
-	}
+	}    
 	return render(request, 'events/events.html', context)
 
 
